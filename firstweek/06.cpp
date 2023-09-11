346d47a072c5ecece205e70464fbd1577f0ba841
Applopie <stina.nekrasova.2@yandex.ru>
2023-09-11 14:42:24 +0300

    booom i am actually slay

---
+++ firstweek/06.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int a)
+{
+    a = 15;
+    ofstream fout("numbers.txt");
+    for (int i = 0; i < a; i++)
+    {
+        fout << i + 1 << ' ';
+    }
+    fout << endl;
+    fout.close();
+    return 0;
+}
