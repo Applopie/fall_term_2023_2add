e0e24003ad7701f8f4121a0b85cf7b2ad24d74e7
Applopie <stina.nekrasova.2@yandex.ru>
2023-09-11 14:05:57 +0300

    i want coffee tho but next

---
+++ firstweek/03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 30; i++)
+    {
+        cout << i + 1 << ' ';
+        // cout << i + 1 << endl;
+    }
+    cout << endl;
+    return 0;
+}
