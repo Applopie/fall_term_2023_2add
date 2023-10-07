ba2903121eddb33c8a3b9e1149b3f69f62202212
Applopie <stina.nekrasova.2@yandex.ru>
2023-10-07 16:12:00 +0300

    lacy oh lacy like ribbons in your hair

--- sandbox.cpp
+++ sandbox.cpp
@@ -8,11 +8,6 @@ using namespace std;
 int main()
 {
     vector<int> digits = {1, 2, 3, 4, 5};
-    vector<int> digits2;
-    digits2 = digits;
-    for (int i = 0; i < 5; i++)
-    {
-        cout << digits2.at(i) << ' ';
-    }
+    cout << digits.size() << endl;
     return 0;
 }
